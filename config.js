8dec266c54b4ae8cd2637ae6b6a42ea9ee3b0be8
Vinpinkley666 <166421298+Vinpink2@users.noreply.github.com>
2025-05-28 13:00:27 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PREFIX: process.env.PREFIX || "JUNE-MD~",
+  EMAIL: process.env.EMAIL || "tylorxxxxxxx@gmail.com",
+  PASS: process.env.PASS || "tylorxxxxx",
+  MESSAGE: process.env.MESSAGE || "*JUNE-MD SESSION BELOW*",
+  IMAGE: process.env.IMAGE || "https://files.catbox.moe/rfc3zo.jpg",
+}
