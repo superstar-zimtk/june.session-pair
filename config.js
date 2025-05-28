df3af1175089ed4f7f7f03c43302beb3ec6b77d0
Vinpinkley666 <166421298+Vinpink2@users.noreply.github.com>
2025-05-28 20:19:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PREFIX: process.env.PREFIX || "JUNE-MD~",
-  EMAIL: process.env.EMAIL || "tylorxxxxxxx@gmail.com",
-  PASS: process.env.PASS || "tylorxxxxx",
+  EMAIL: process.env.EMAIL || "vincentmaneno65@gmail.com ",
+  PASS: process.env.PASS || "vinpink254*",
   MESSAGE: process.env.MESSAGE || "*JUNE-MD SESSION BELOW*",
   IMAGE: process.env.IMAGE || "https://files.catbox.moe/rfc3zo.jpg",
 }
