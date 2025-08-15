cf9aa1fae1941f33147e78f8938d02d50e9d0c18
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-08-15 15:30:24 +0300

    Add files via upload

---
+++ pair.js
@@ -0,0 +1,86 @@
+const PastebinAPI = require('pastebin-js');
+const pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
+const { makeid } = require('./id');
+const express = require('express');
+const fs = require('fs');
+const pino = require('pino');
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    version,
+    delay,
+    makeCacheableSignalKeyStore,
+} = require("@whiskeysockets/baileys");
+
+const router = express.Router();
+
+// Helper function to remove files
+function removeFile(filePath) {
+    if (!fs.existsSync(filePath)) return false;
+    fs.rmSync(filePath, { recursive: true, force: true });
+}
+
+// Route handler
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+
+    async function RAVEN() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
+      const client = makeWASocket({
+        printQRInTerminal: false,
+        version: [2, 3000, 1023223821],
+        logger: pino({
+          level: 'silent',
+        }),
+        browser: ['Ubuntu', 'Chrome', '20.0.04'],
+        auth: state,
+      })
+
+            if (!client.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await client.requestPairingCode(num);
+
+                 if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            client.ev.on('creds.update', saveCreds);
+            client.ev.on('connection.update', async (s) => {
+                const { connection, lastDisconnect } = s;
+                if (connection === 'open') {
+                await client.sendMessage(client.user.id, { text: `Session generation in progress,Please Wait...🖋️` });
+                    await delay(1500);
+                    
+                    const data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    await delay(8000);
+                    const b64data = Buffer.from(data).toString('base64');
+                    const session = await client.sendMessage(client.user.id, { text: 'JUNE:~' + b64data });
+
+                    // Send message after session
+                    await client.sendMessage(client.user.id, {text: "✅ Session paired successfully!\nTYPE: BASE64\nSTATUS: Active & Working...🖋️" }, { quoted: session });
+                    
+                    await delay(100);
+                    await client.ws.close();
+                    removeFile('./temp/' + id);
+                } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
+                    await delay(10000);
+                    RAVEN();
+                }
+            });
+        } catch (err) {
+            console.log('service restarted', err);
+            removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: 'Service Currently Unavailable' });
+            }
+        }
+    }
+
+    await RAVEN();
+});
+
+module.exports = router;
